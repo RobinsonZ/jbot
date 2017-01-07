394f271685d63d6b96000153b3cd24d44119bb19
RobinsonZ <robinsonz@catlin.edu>
2017-01-07 09:57:58 -0800

    Added method with no parameters passed to the server

--- jbot/src/main/java/me/ramswaroop/jbot/core/slack/SlackWebAPI.java
+++ jbot/src/main/java/me/ramswaroop/jbot/core/slack/SlackWebAPI.java
@@ -1,12 +1,15 @@
 package me.ramswaroop.jbot.core.slack;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Map;
 
 import org.apache.http.client.utils.URIBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 
 import com.fasterxml.jackson.core.JsonParseException;
@@ -18,6 +21,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  * @author Zachary Robinson
  */
 public class SlackWebAPI {
+	private static final Logger logger = LoggerFactory.getLogger(SlackWebAPI.class);
 	
 	@Value("${slackBotToken}")
 	private static String token;
@@ -27,11 +31,11 @@ public class SlackWebAPI {
 	/** Makes a request to the Slack API with the defined method and params.
 	 * @param method The Slack method to call.
 	 * @param params A map containing the paremeters that make up the request.
-	 * @return A map of the returned 
+	 * @return A Map containing the returned values as described by the Slack Web API method documentation.
+	 * @throws IOException If there is an IO error between the client and API server.
 	 */
 	@SuppressWarnings("unchecked")
-	public static Map<String, Object> makeApiRequest(String method, Map<String, String> params) {
-		//TODO: Add logging
+	public static Map<String, Object> makeApiRequest(String method, Map<String, String> params) throws IOException {
 		ObjectMapper mapper = new ObjectMapper();
 		
 		URIBuilder requestBuilder = new URIBuilder()
@@ -41,16 +45,18 @@ public class SlackWebAPI {
 		
 		requestBuilder.addParameter("token", token);
 		
-		for (Map.Entry<String, String> param : params.entrySet()) {
-			requestBuilder.addParameter(param.getKey(), param.getValue());
+		if (params != null) {
+			for (Map.Entry<String, String> param : params.entrySet()) {
+				requestBuilder.addParameter(param.getKey(), param.getValue());
+			}
 		}
 		
 		URI requestURI;
+		
 		try {
 			requestURI = requestBuilder.build();
-		} catch (URISyntaxException e) {
-			requestURI = null;
-			// TODO add some logging for this, though it shouldn't happen
+		} catch (URISyntaxException e) { //No need to handle–syntax is hardcoded.
+			requestURI = null; //This is just here to make the compiler happy
 		}
 		
 		Map<String, Object> response = null;
@@ -62,11 +68,21 @@ public class SlackWebAPI {
 		} catch (JsonMappingException e) {
 			// TODO write logging code
 			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO write logging code
-			e.printStackTrace();
+		}
+		
+		if (!Boolean.getBoolean(response.get("ok").toString())) {
+			logger.error("Slack Web API returned error: " + response.get("error"));
 		}
 		
 		return response;
+	}
+	
+	/** Makes a request to the Slack Web API with the defined method and no params.
+	 * @param method The method to be called
+	 * @return A map containing the returned values as described in the Slack API documentation
+	 * @throws IOException if there is an IO error between the client and server.
+	 */
+	public static Map<String, Object> makeApiRequest(String method) throws IOException {
+		return makeApiRequest(method, null);
 	}
 }
