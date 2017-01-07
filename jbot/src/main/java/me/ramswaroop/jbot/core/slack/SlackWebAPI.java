bd41ef9f9e7380fe9240179b0e3449d529976d2f
RobinsonZ <robinsonz@catlin.edu>
2017-01-07 09:59:34 -0800

    removed unnecessary import

--- jbot/src/main/java/me/ramswaroop/jbot/core/slack/SlackWebAPI.java
+++ jbot/src/main/java/me/ramswaroop/jbot/core/slack/SlackWebAPI.java
@@ -1,7 +1,6 @@
 package me.ramswaroop.jbot.core.slack;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
